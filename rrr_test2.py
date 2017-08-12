505cbe327f280af8b9078107fc84b44fa9cdb88f
rcube777 <rohit454649@gmail.com>
2017-08-11 17:30:10 -0700

    [Second Commit]

--- rrr_test2.py
+++ rrr_test2.py
@@ -6,13 +6,15 @@ from sklearn.utils import shuffle
 import scipy
 
 samples = []
-with open('C:/Users/rranade/Documents/GitHub/CarND-Behavioral-Cloning-P3/windows_sim/windows_sim/windows_sim_Data/IMG/August9th/driving_log.csv') as csvfile:
+with open('C:/Users/rranade/Documents/GitHub/CarND-Behavioral-Cloning-P3/windows_sim/windows_sim/windows_sim_Data/IMG/August10th/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         samples.append(line)
 
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+print(np.shape(train_samples))
 
 def intensity_change(image):
 	im = cv2.cvtColor(image,cv2.COLOR_RGB2HSV)
@@ -24,13 +26,13 @@ def intensity_change(image):
 	im = cv2.cvtColor(im,cv2.COLOR_HSV2RGB)
 	return im
 
-def view_change(image,s,range):
-	xc = range*np.random.uniform() - range/2
-	sang = s + xc/range*2*.2
-	yx = 40*np.random_uniform() - 40/2
-	TM =np.float32([1,0,xc],[0,1,xy])
-	image_vc = cv2.warpAffine(image,TM,(90,320))
-
+def view_change(image,s,trange):
+	xc = trange*np.random.uniform() - trange/2
+	sang = s + xc/trange*2*.2
+	yx = 40*np.random.uniform() - 40/2
+	TM =np.float32([[1,0,xc],[0,1,yx]])
+	image_vc = cv2.warpAffine(image,TM,(160,320))
+	# print(np.shape(image_vc))
 	return image_vc,sang
 
 def generator(samples, batch_size=32):
@@ -49,36 +51,46 @@ def generator(samples, batch_size=32):
                 center_image = cv2.imread(name)
                 #center_image = center_image[50:140,:,:]
                 center_angle = float(batch_sample[3])
+                #center_image = scipy.misc.imresize(center_image,(64,64,3))
                 images.append(center_image)
                	angles.append(center_angle)
 
+               	# image_vc,angle_vc = view_change(center_image,center_angle,100)
+               	# print(np.shape(image_vc))
+               	# images.append(image_vc)
+               	# angles.append(angle_vc)
                 image_flip = np.fliplr(center_image)
                 images.append(image_flip)
                 angles.append(center_angle*-1.0)
 
                 image_ic = intensity_change(center_image)
+                #image_ic = scipy.misc.imresize(image_ic,(64,64,3))
                 images.append(image_ic)
                 angles.append(center_angle)
 
                 name = batch_sample[1].split('/')[-1]
                 left_image = cv2.imread(name)
+                #left_image = scipy.misc.imresize(left_image,(64,64,3))
                 #left_image = left_image[50:140,:,:]
                 #left_image = cv2.resize(left_image,(64,64,3))
                 images.append(left_image)
                 angles.append(center_angle + 0.25)
 
                 image_il = intensity_change(left_image)
+                #image_il = scipy.misc.imresize(image_il,(64,64,3))
                 images.append(image_il)
                 angles.append(center_angle + 0.25)
 
                 name = batch_sample[2].split('/')[-1]
                 right_image = cv2.imread(name)
+                #right_image = scipy.misc.imresize(right_image,(64,64,3))
                 #right_image = right_image[50:140,:,:]
                 #right_image = cv2.resize(right_image,(64,64,3))
                 images.append(right_image)
                 angles.append(center_angle - 0.25)
 
                 image_ir = intensity_change(right_image)
+                #image_ir = scipy.misc.imresize(image_ir,(64,64,3))
                 images.append(image_ir)
                 angles.append(center_angle - 0.25)
                 #print(batch_sample[1])
@@ -93,16 +105,25 @@ def generator(samples, batch_size=32):
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
+def resize_image(image):
+	import tensorflow as tf
+	return tf.image.resize_images(image,(64,64))
 #print('Test')
 #print (np.size(train_samples))
 from keras.models import Sequential, load_model
-from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout, Reshape
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
+
 model = Sequential()
-model.add(Lambda(lambda x : (x/255.0), input_shape = (160,320,3)))
+model.add(Lambda(lambda x : (x/255.0 - 0.5), input_shape = (160,320,3)))
 model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+model.add(Lambda(resize_image, name = 'ResizeImage'))
+#model.add(Lambda(lambda x : (x/255.0), input_shape = (64,64,3)))
+#model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+# model.add(Lambda(lambda x : tf.image.resize_images(x,(64,64))))
+#model.add(Reshape((64,64,3), input_shape = (90,320,3)))
 #model.add(Lambda(lambda x : (x/255.0), input_shape = (90,320,3)))
 #Input = 160x320x3. Output = 166x316x6
 model.add(Convolution2D(32,5,5,activation = 'relu')) #TODO
@@ -111,18 +132,18 @@ model.add(MaxPooling2D(pool_size = (2,2))) #TODO
 #Input = 83x158x6. Output = 79x154x16 
 model.add(Convolution2D(64,5,5,activation = 'relu'))
 #Input= 79x154x16. Output = 38x77x16
-model.add(MaxPooling2D())
+model.add(MaxPooling2D(pool_size = (2,2)))
 #Input = 38x77x16.
 model.add(Convolution2D(128,5,5,activation = 'relu'))
-model.add(Dropout(0.5))
+# model.add(Dropout(0.5))
 model.add(Flatten())
-model.add(Dense(1024))
+model.add(Dense(1024, activation = 'relu'))
 model.add(Dropout(0.5))
-model.add(Dense(512))
-model.add(Dropout(0.5))
-model.add(Dense(64))
+model.add(Dense(512, activation = 'relu'))
+# model.add(Dropout(0.5))
+model.add(Dense(64, activation = 'relu'))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=7)
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=1)
 model.save('model_test2.h5')
